9e110fae55812854e456aa52ba8b4f14ab9ff977
Jared Wasserman <jaredbwasserman@gmail.com>
2023-12-31 14:34:33 -0800

    Add test case

--- src/test/java/com/jaredbwasserman/openfda/util/PermutationUtilTest.java
+++ src/test/java/com/jaredbwasserman/openfda/util/PermutationUtilTest.java
@@ -35,7 +35,34 @@ public class PermutationUtilTest {
                         List.of(3, 2)
                 ),
                 List.of(0),
-                List.of()
+                List.of(),
+                List.of(2, 3, 4),
+                List.of(
+                        List.of(0, 0, 0),
+                        List.of(0, 0, 1),
+                        List.of(0, 0, 2),
+                        List.of(0, 0, 3),
+                        List.of(0, 1, 0),
+                        List.of(0, 1, 1),
+                        List.of(0, 1, 2),
+                        List.of(0, 1, 3),
+                        List.of(0, 2, 0),
+                        List.of(0, 2, 1),
+                        List.of(0, 2, 2),
+                        List.of(0, 2, 3),
+                        List.of(1, 0, 0),
+                        List.of(1, 0, 1),
+                        List.of(1, 0, 2),
+                        List.of(1, 0, 3),
+                        List.of(1, 1, 0),
+                        List.of(1, 1, 1),
+                        List.of(1, 1, 2),
+                        List.of(1, 1, 3),
+                        List.of(1, 2, 0),
+                        List.of(1, 2, 1),
+                        List.of(1, 2, 2),
+                        List.of(1, 2, 3)
+                )
         );
 
         for (final Map.Entry<List<Integer>, List<List<Integer>>> inputOutputEntry : inputOutput.entrySet()) {
